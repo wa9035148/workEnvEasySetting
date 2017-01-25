2fe85fb13cc33fd31b0f4301eef83de59639a97c
wang.hao <wang.hao@megachips.co.jp>
2017-01-25 16:51:20 +0900

    add verilog mode install script

--- install.sh
+++ install.sh
@@ -14,3 +14,9 @@ ln -s $install_dir/vim .vim
 ln -s $install_dir/vimrc .vimrc
 ln -s $install_dir/tmux-conf/tmux.conf .tmux.conf
 
+#install verilog mode 
+mkdir -p $HOME/elisp
+wget https://www.veripool.org/ftp/verilog-mode.el $HOME/elisp/ 
+mkdir -p $HOME/.vim/ftplugin/
+wget http://www.vim.org/scripts/download_script.php?src_id=7578 $HOME/.vim/ftplugin/
+
