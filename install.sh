8f3d196bb71b8fa8ea2b1f15c332bd8875e643a9
wang.hao <wang.hao@megachips.co.jp>
2017-02-07 10:25:24 +0900

    debug

--- install.sh
+++ install.sh
@@ -6,17 +6,10 @@ install_dir=pwd
 
 git submodule update --init --recursive
 
-./vim/bundle/YouCompleteMe/.install.py
+#./vim/bundle/YouCompleteMe/.install.py
 
 cd $HOME
 
-ln -s $install_dir/vim .vim
-ln -s $install_dir/vimrc .vimrc
-ln -s $install_dir/tmux-conf/tmux.conf .tmux.conf
-
-#install verilog mode 
-mkdir -p $HOME/elisp
-wget https://www.veripool.org/ftp/verilog-mode.el $HOME/elisp/ 
-mkdir -p $HOME/.vim/ftplugin/
-wget http://www.vim.org/scripts/download_script.php?src_id=7578 $HOME/.vim/ftplugin/
-
+ln -sf $install_dir/vim .vim
+ln -sf $install_dir/vimrc .vimrc
+ln -sf $install_dir/tmux-conf/tmux.conf .tmux.conf
